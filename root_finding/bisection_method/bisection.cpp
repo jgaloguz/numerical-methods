9e85cdc05dab298a4d2e13bfa1b40e092051cbe5
Juan G Alonso Guzman <33181561+jgaloguz@users.noreply.github.com>
2020-03-10 11:12:26 -0500

    Upload root finding methods
    Upload bisection, secant, and regula-falsi.

---
+++ root_finding/bisection_method/bisection.cpp
@@ -0,0 +1,76 @@
+// Bisection method for root finding
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <cmath>
+#include <string>
+using namespace std;
+
+// Define function for the function (polynomial) to be integrated
+float f(float x)
+{
+	// Function 2 - e^x
+	return 2 - exp(x);
+}
+
+
+int main() {
+	// Variable definition
+	int i, j, k, N;
+	float a, b, c, R;
+	float fc;
+	float tol = 1e-6;
+	float ntol = -1*tol;
+
+	// Input basic parameter
+	cout << "Input left bound for interval: ";		// Ask for left bound of interval
+	cin >> a;
+	cout << "Input right bound for interval: ";		// Ask for right bound of interval
+	cin >> b;
+	
+	// Make sure there is at least one solution in the given interval
+	if(f(a)*f(b) > 0)
+	{
+		cout << "Both end points for the given interval have the same sign." << endl;
+		cout << "Cannot guarantee root exists within the given interval." << endl;
+		cout << "Please provide endpoints with opposite signs." << endl;
+		return 0;
+	}
+
+	cout << "Input maximum number of iterations: "; 	// Ask for number of iterations
+	cin >> N;
+
+	// Begin iterative process
+	for(i = 0; i < N; i++)
+	{
+		c = (a + b)/2;
+		fc = f(c);
+		if(ntol < fc && fc < tol)
+		{
+			break;					// Solution found within specified tolerance
+		}
+		else if (f(a)*fc < 0)
+		{
+			b = c;					// Update [a,b] --> [a,c]
+		}
+		else
+		{
+			a = c;					// Update [a,b] --> [c,b]
+		}
+	}
+
+	// Output results
+	if(i == N)						// Max number of iterations completed
+	{
+		cout << "The root could not be approximated to within " << tol << " tolerance in " << N << " iteration(s)." << endl;
+		cout << "The current best guess is " << c << " yielding a function value of " << fc << "." << endl;
+	}
+	else
+	{
+		cout << "The root was approximated to within " << tol << " tolerance in " << i+1 << " iteration(s)." << endl;
+		cout << "The root is " << c << " yielding a function value of " << fc << "." << endl;
+	}
+
+	return 0; // End of the program
+}
