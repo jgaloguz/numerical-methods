416bac631aea6f072bd5578ad59568eb1c0f984c
Juan G Alonso Guzman <33181561+jgaloguz@users.noreply.github.com>
2020-08-28 09:44:47 -0500

    Upload Simpson's Rule
    Upload Simpson's Rule

---
+++ numer_integ/simpson_rule/README.txt
@@ -0,0 +1,9 @@
+Simpson's Rule Algorithm: approximate a definite integral using Simpson's rule.
+
+simpson_rule.cpp input(s):
+	- Lower and upper bounds of integration, a, b (user-input).
+	- Function to be integrated, f(x) (hard-coded).
+	- Number of grid points to utilize, N (user-input).  Must be at least 2 and even.
+
+simpson_rule.cpp output(s):
+	- Approximated value of the integral (terminal-print).
---
+++ numer_integ/simpson_rule/simpson_rule.cpp
@@ -0,0 +1,47 @@
+// Simpson's Rule for approximating definite integrals
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <cmath>
+#include <string>
+using namespace std;
+
+// Define function for the function to be integrated
+float f(float x)
+{
+	// Function 1 - x + 3x^2 + (7/2)x^3
+	return 1 - x + 3*pow(x,2) + 3.5*pow(x,3);
+}
+
+int main() {
+	// Variable definition
+	int i, j, k, N, M;
+	float a, b, dx, x;
+	float S = 0;
+
+	// Input basic parameter
+	cout << "Input lower bound of integration: "; 	// Ask for lower bound
+	cin >> a;
+	x = a;						// Set initial point at a
+	cout << "Input upper bound of integration: "; 	// Ask for upper bound
+	cin >> b;
+	cout << "Input number of grid points: "; 	// Ask for number of grid points
+	cin >> N;
+	M = N-1;					// Compute number of intervals
+	dx = (b - a)/M;					// Compute step size
+
+	// Approximate solution
+	for(i = 1; i < M; i++)
+	{
+		x += dx;
+		S += (i % 2 ? 4.0/3 : 2.0/3) * f(x);
+	}
+	S += (f(a) + f(b))/3;
+	S *= dx;
+
+	// Output value
+	cout << "Integral = " << S << endl;
+
+	return 0; // End of the program
+}
