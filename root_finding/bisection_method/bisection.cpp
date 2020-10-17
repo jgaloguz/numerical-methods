2c6f78ce364be4fe2312e99fa9b759ca62a8c99e
Juan G Alonso Guzman <33181561+jgaloguz@users.noreply.github.com>
2020-10-17 16:03:36 -0500

    Update bisection.cpp
    Added some comments.

--- root_finding/bisection_method/bisection.cpp
+++ root_finding/bisection_method/bisection.cpp
@@ -7,7 +7,7 @@
 #include <string>
 using namespace std;
 
-// Define function for the function (polynomial) to be integrated
+// Define function for which to find root
 float f(float x)
 {
 	// Function 2 - e^x
@@ -17,11 +17,12 @@ float f(float x)
 
 int main() {
 	// Variable definition
-	int i, j, k, N;
-	float a, b, c, R;
-	float fc;
-	float tol = 1e-6;
-	float ntol = -1*tol;
+	int i, j, k;						// dummy indices
+	int N;							// maximum number of iterations
+	float a, b, c;						// left, right, and mid-points of (sub)intervals
+	float fc;						// value of function at midpoint
+	float tol = 1e-6;					// accuracy tolerance (positive)
+	float ntol = -1*tol;					// accuracy tolerance (negative)
 
 	// Input basic parameter
 	cout << "Input left bound for interval: ";		// Ask for left bound of interval
@@ -44,29 +45,29 @@ int main() {
 	// Begin iterative process
 	for(i = 0; i < N; i++)
 	{
-		c = (a + b)/2;
-		fc = f(c);
+		c = (a + b)/2;					// find mid point of interval [a,b]
+		fc = f(c);					// find value of function at midpoint
 		if(ntol < fc && fc < tol)
 		{
 			break;					// Solution found within specified tolerance
 		}
-		else if (f(a)*fc < 0)
+		else if (f(a)*fc < 0)				// f(c) and f(b) have the same sign
 		{
 			b = c;					// Update [a,b] --> [a,c]
 		}
-		else
+		else						// f(c) and f(a) have the same sign
 		{
 			a = c;					// Update [a,b] --> [c,b]
 		}
 	}
 
 	// Output results
-	if(i == N)						// Max number of iterations completed
+	if(i == N)						// Max number of iterations completed (solution not found)
 	{
 		cout << "The root could not be approximated to within " << tol << " tolerance in " << N << " iteration(s)." << endl;
 		cout << "The current best guess is " << c << " yielding a function value of " << fc << "." << endl;
 	}
-	else
+	else							// solution found
 	{
 		cout << "The root was approximated to within " << tol << " tolerance in " << i+1 << " iteration(s)." << endl;
 		cout << "The root is " << c << " yielding a function value of " << fc << "." << endl;
