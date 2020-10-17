7002b2bec8857207d4aab75ad0ab905afaeed503
Juan G Alonso Guzman <33181561+jgaloguz@users.noreply.github.com>
2020-10-17 16:07:46 -0500

    Update newton.cpp
    Added some comments.

--- root_finding/newton_method/newton.cpp
+++ root_finding/newton_method/newton.cpp
@@ -7,7 +7,7 @@
 #include <string>
 using namespace std;
 
-// Define function
+// Define function for which to find a root
 float f(float x)
 {
 	// Function 2 - e^x
@@ -23,13 +23,15 @@ float df(float x)
 
 int main() {
 	// Variable definition
-	int i, j, k, N;
-	float a, x, fa;
-	float tol = 1e-6;
-	float ntol = -1*tol;
+	int i, j, k;						// dummy indices
+	int N;							// maximum number of interations
+	float a, x;						// approximate root, and new guess
+	float fa;						// value of function at approximate root
+	float tol = 1e-6;					// accuracy tolerance (positive)
+	float ntol = -1*tol;					// accuracy tolerance (negative)
 
 	// Input basic parameter
-	cout << "Approximate solution: ";					// Ask for initial guess
+	cout << "Approximate solution: ";			// Ask for initial guess
 	cin >> a;
 
 	cout << "Input maximum number of iterations: "; 	// Ask for number of iterations
@@ -38,23 +40,23 @@ int main() {
 	// Begin iterative process
 	for(i = 0; i < N; i++)
 	{
-		fa = f(a);
+		fa = f(a);					// find value of function at approximate root
 		if(ntol < fa && fa < tol) {
-			break;
+			break;					// root has been found to desired accuracy
 		}
 		else {
-			x = a - fa/df(a);
+			x = a - fa/df(a);			// find root of f linearized at a
 			a = x;					// Update a --> x
 		}
 	}
 
 	// Output results
-	if(i == N)						// Max number of iterations completed
+	if(i == N)						// Max number of iterations completed (solution not found)
 	{
 		cout << "The root could not be approximated to within " << tol << " tolerance in " << N << " iteration(s)." << endl;
 		cout << "The current best guess is " << a << " yielding a function value of " << fa << "." << endl;
 	}
-	else
+	else							// solution found
 	{
 		cout << "The root was approximated to within " << tol << " tolerance in " << i+1 << " iteration(s)." << endl;
 		cout << "The root is " << a << " yielding a function value of " << fa << "." << endl;
