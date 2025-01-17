13760bec00d5e94cf698cbb6d0ecf90a6e4ed06b
Juan G Alonso Guzman <33181561+jgaloguz@users.noreply.github.com>
2025-01-17 11:52:14 -0600

    Upload MCMC method for Ising model
    Upload MCMC method for finding the critical temperature of a 2D Ising-lattice.

---
+++ MCMC/Ising_model/README.md
@@ -0,0 +1,8 @@
+*Markov Chain Monte Carlo (MCMC)*
+An MCMC method applied to finding the critical (phase-change) temperature of a square 2D Ising-lattice of locally, isotropically interacting magnetic spins without an external magnetic field.
+
+ising_model.cpp input(s):
+   - Number of lattice sites per side of the square lattice, `N` (hard-coded).
+
+ising_model.cpp output(s):
+   - `solution.txt` file containing the average magnetization for a range of temperatures. The temperature, `T`, at which the magnetization, `M`, suddenly decreases from ~1 to ~0 is the critical temperature. This file may be directly inputed to any suitable 2D graphing software for vizualization, such as xmgrace.
---
+++ MCMC/Ising_model/ising_model.cpp
@@ -0,0 +1,95 @@
+// MCMC applied to the problem of 2D Ising-lattice magnetization
+
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <cstdlib>
+using namespace std;
+
+#define N 100        // Length of either side of 2D lattice
+
+// Function for computing the total, signed magnetization of a lattice
+long TotalSignedMagnetization(double lattice[N][N])
+{
+   int i,j;          // dummy indices
+   long M = 0;       // average magnetization
+   for (i = 0; i < N; i++) {
+      for (j = 0; j < N; j++) {
+         M += (long)lattice[i][j];
+      };
+   };
+   return M;
+};
+
+// Function to evolve the Markov Chain based on the Metropolis-Hastings algorithm
+void MarkovChain(double lattice[N][N], double T, long &tsM)
+{
+   int i = drand48() * N;        // row of cell of Ising-lattice to flip
+   int j = drand48() * N;        // column of cell of Ising-lattice to flip
+
+   int i_pls = (i+1) % N;        // row of right neighbor cell
+   int i_mns = (i-1) % N;        // row of left neighbor cell
+   int j_pls = (j+1) % N;        // row of top neighbor cell
+   int j_mns = (j-1) % N;        // row of bottom neighbor cell
+
+   double H_ij = lattice[i][j] * ( lattice[i_pls][j] + lattice[i_mns][j]
+                                 + lattice[i][j_pls] + lattice[i][j_mns]); // local binding energy
+
+// NOTE: if H = {(positive) sum over product of neighboring spins}, then
+// pi_j / pi_i = exp[(H(j)-H(i))/T] for current state i and potential state j.
+// Since the potential state is just a single spin flip, then
+// H(j)-H(i) = -2.0 * {local energy of current spin}, prior to flip
+   double acc_prob = fmin(1.0, exp(-2.0 * H_ij / T)); // acceptance probability (Metropolis-Hastings)
+
+   if (drand48() < acc_prob) {               // if potential state is accepted
+      lattice[i][j] *= -1.0;                 // flip spin
+      tsM += 2 * (long)lattice[i][j];        // {new tsM} = {old tSM} + 2 * {new spin state}
+   };
+};
+
+int main() {
+
+   double lattice[N][N];                     // 2D Ising-lattice
+   double M;                                 // Average (unsigned) magnetization
+   long tuM;                                 // Total unsigned magnetization
+   long tsM;                                 // Total signed magnetization
+   double T;                                 // Temperature
+   double T_low = 0.1;                       // Lowest temperature to search
+   double T_high = 5.0;                      // Highest temperature to search
+   double N_T = 100;                         // Number of temperatures to try
+   double dT = (T_high - T_low) / (N_T-1);   // Temperature increment
+   long burn_in_time = 10 * N * N;           // Burn-in time
+   long MC_trials = 100 * N * N;             // Number of MC samples from stationary distribution
+   long i,j;                                 // dummy indices
+   srand48(time(NULL));                      // seed random number generator
+   ofstream solution_file;                   // solution file
+
+// Initial condition (uniform), technically the most likely configuration at ANY temperature
+   for (i = 0; i < N; i++) {
+      for (j = 0; j < N; j++) {
+         lattice[i][j] = 1.0;
+      };
+   };
+   tsM = TotalSignedMagnetization(lattice);     // Initialize total, signed magnetization
+
+   solution_file.open("solution.txt");          // Open output file
+// Loop over temperatures
+   T = T_low;
+   for (i = 0; i < N_T; i++) {
+      cout << T << endl;
+// Evolve chain for the burn-in time
+      for (j = 0; j < burn_in_time; j++) MarkovChain(lattice, T, tsM);
+// Find average magnetization using Monte Carlo method
+      tuM = 0;
+      for (j = 0; j < MC_trials; j++) {
+         MarkovChain(lattice, T, tsM);
+         tuM += (tsM > 0 ? tsM : -tsM);
+      };
+      M = (double)tuM / (double)(N * N * MC_trials);
+      solution_file << T << " " << M << endl;   // Output values to file
+      T += dT;
+   };
+   solution_file.close();                       // Close output file
+   
+   return 0; // End of the program
+}
